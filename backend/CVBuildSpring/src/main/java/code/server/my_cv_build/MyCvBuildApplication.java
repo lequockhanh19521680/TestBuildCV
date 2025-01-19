f499ee33d3366747186b5e93f38811bef1ed5f03
khanhcm0901 <19521680@gm.uit.edu.vn>
2025-01-19 21:00:36 +0700

    build init project

---
+++ backend/CVBuildSpring/src/main/java/code/server/my_cv_build/MyCvBuildApplication.java
@@ -0,0 +1,13 @@
+package code.server.my_cv_build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCvBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCvBuildApplication.class, args);
+	}
+
+}
---
+++ backend/CVBuildSpring/src/test/java/code/server/my_cv_build/MyCvBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package code.server.my_cv_build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCvBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
